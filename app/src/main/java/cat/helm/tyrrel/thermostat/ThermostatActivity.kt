8c31958532723dc1ca8cd929ec79dea2af074c12
Barodroid <hussein@elbaroudy.net>
2017-11-09 00:33:37 +0100

    Migrate ThermostatActivity from Java to Kotlin

---
+++ app/src/main/java/cat/helm/tyrrel/thermostat/ThermostatActivity.kt
@@ -0,0 +1,11 @@
+package cat.helm.tyrrel.thermostat
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ThermostatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
