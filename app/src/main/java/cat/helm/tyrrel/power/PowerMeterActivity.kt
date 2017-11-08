6250e4780ec9e39b96a633d26d933e05fa35335c
Barodroid <hussein@elbaroudy.net>
2017-11-09 00:32:09 +0100

    Migrate PowerMeterActivity from Java to Kotlin

---
+++ app/src/main/java/cat/helm/tyrrel/power/PowerMeterActivity.kt
@@ -0,0 +1,28 @@
+package cat.helm.tyrrel.power
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import cat.helm.tyrrel.R
+import kotlinx.android.synthetic.main.activity_except_schedule.*
+
+
+class PowerMeterActivity : AppCompatActivity() {
+
+    companion object {
+
+        fun start(context: Activity) {
+            val intent = Intent(context, PowerMeterActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_power_metter)
+        setSupportActionBar(toolbar)
+
+    }
+
+}
